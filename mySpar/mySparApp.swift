1b731fd688a7a95a13195893d53847ad200b4755
AntonR8 <129757394+AntonR8@users.noreply.github.com>
2024-08-20 10:22:03 +0300

    Initial Commit

---
+++ mySpar/mySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  mySparApp.swift
+//  mySpar
+//
+//  Created by Антон Разгуляев on 20.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct mySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
